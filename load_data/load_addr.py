2be89c31b1c1d1eddadcbd1e571cb012112b95ec
Hao-JH <129087667+Hao-JH@users.noreply.github.com>
2023-10-21 15:10:13 +0800

    add date

--- load_data/load_addr.py
+++ load_data/load_addr.py
@@ -4,6 +4,7 @@ import torch
 import pandas as pd
 import time
 from tqdm import tqdm
+import datetime
 
 
 current_folder = os.path.dirname(os.path.abspath(__file__))
@@ -15,11 +16,20 @@ addr_dict = pd.read_csv('../data/addr_feat.csv')
 # 选择要作为节点特征的列
 feature_cols = ['all_hash_cnt', 'all_date_cnt', 'all_method_cnt', 'all_to_addr_cnt',
                  'sum_actual_fee', 'first_nonce', 'last_nonce',
+                  'all_min_dt', 'all_max_dt','first_block_date','last_block_date',
                  'eth_count_out_sum', 'eth_count_out_avg', 'eth_count_out_min', 'eth_count_out_max']
 
 date_cols = ['all_min_dt', 'all_max_dt','first_block_date','last_block_date']
 
 features = addr_dict[feature_cols]
+
+for col in date_cols:
+    # 将字符串类型的日期转换为datetime对象
+    date_obj = pd.to_datetime(features[col], format="%Y-%m-%d")
+    # 将datetime对象转换为时间戳并除以100
+    timestamp = (date_obj - datetime.datetime(1970, 1, 1)).dt.total_seconds() / 100
+    # 将结果赋值给原始数据集
+    features[col] = timestamp.astype(float)
 
 # for col in tqdm(date_cols):
 #     for element in features[col]:
---
+++ load_data/load_label.py
@@ -0,0 +1,6 @@
+import numpy as np
+import os
+import torch
+import pandas as pd
+import time
+from tqdm import tqdm
