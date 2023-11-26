c0027fedd4ce44b26aca003e4abbee98b3d15475
Hao-JH <129087667+Hao-JH@users.noreply.github.com>
2023-11-26 09:58:16 +0800

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -7,19 +7,17 @@ labels = np.array(cluster_id)
 cluster_result = pd.read_csv('clustered_result.csv')
 result = np.array(cluster_result)
 
-# 计算混淆矩阵
-tp = np.sum((labels != -1) & (result != -1))  # 真正例
-fp = np.sum((labels == -1) & (result != -1))  # 假正例
-tn = np.sum((labels == -1) & (result == -1))  # 真负例
-fn = np.sum((labels != -1) & (result == -1))  # 假负例
+tp = np.sum((labels != -1) & (result != -1))   
+fp = np.sum((labels == -1) & (result != -1))  
+tn = np.sum((labels == -1) & (result == -1))  
+fn = np.sum((labels != -1) & (result == -1))  
 
-
-# 计算准确率
+# Calculate precision
 precision = tp / (tp + fp)
 
-# 计算召回率
+# Calculate recall
 recall = tp / (tp + fn)
 
-# 打印结果
-print("准确率:", precision)
-print("召回率:", recall)
+# Print the results
+print("Precision:", precision)
+print("Recall:", recall)
